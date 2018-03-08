6042f4d223ffee9f2325eb5ef7e33b21acf3c677
nahome <nahom.teshome@yahoo.com>
2018-03-08 02:06:36 -0800

    posts and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,16 @@
 
 require 'random_data'
 
+5.times do
+   User.create!(
+ # #3
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+
 15.times do
   Topic.create!(
     name:         RandomData.random_sentence,
@@ -18,6 +28,7 @@ topics = Topic.all
 
  50.times do
    Post.create!(
+     user: users.sample,
      topic: topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -32,9 +43,15 @@ topics = Topic.all
    )
  end
 
+ user = User.first
+  user.update_attributes!(
+    email: 'nahom.teshome@yahoo.com', # replace this with your personal email
+    password: 'helloworld'
+  )
 
 
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+     it { is_expected.to have_many(:posts) }
      # Shoulda tests for name
      it { is_expected.to validate_presence_of(:name) }
      it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -32,6 +33,12 @@ RSpec.describe User, type: :model do
 
      it "should be an invalid user due to blank email" do
        expect(user_with_invalid_email).to_not be_valid
+     end
+
+     it "should format the user's name" do
+       user.name = "bloc user"
+       user.save
+       expect(user.name).to eq "Bloc User"
      end
 
    end
