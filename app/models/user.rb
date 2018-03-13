db0481ebd11bcccb086e5e3eb2665f0be37cbc52
nahome <nahom.teshome@yahoo.com>
2018-03-12 20:46:32 -0700

    user model redo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :member }
   before_save :format_name
 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -11,13 +16,19 @@ class User < ActiveRecord::Base
              length: { minimum: 3, maximum: 254 }
 
    has_secure_password
+   enum role: [:member, :admin]
+
+   def favorite_for(post)
+     favorites.where(post_id: post.id).first
+   end
+
+   private
+
    def format_name
      if name
        name_array = []
-       name.split.each do |name_part|
-         name_array << name_part.capitalize
-       end
-       self.name = name_array.join(" ")
+       name.split.map! { |name_part| name_part.capitalize }
+       self.name = name.join(" ")
      end
    end
 end
