e7d726d8321bcf6d86f28292b5655ab88966f439
nahome <nahom.teshome@yahoo.com>
2018-03-14 21:50:56 -0700

    public profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,34 +1,28 @@
 class User < ActiveRecord::Base
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :votes, dependent: :destroy
-  has_many :favorites, dependent: :destroy
-  before_save { self.email = email.downcase if email.present? }
-  before_save { self.role ||= :member }
-  before_save :format_name
+    has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_many :votes, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+    has_many :favorite_posts, through: :favorites, source: :post, dependent: :destroy
+    before_save { self.email = email.downcase if email.present? }
+    before_save { self.role ||= :member }
+    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+    validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
+    validates :password, length: { minimum: 6 }, allow_blank: true
+    validates :email,
+        presence: true,
+        uniqueness: { case_sensitive: false },
+        length: { minimum: 3, maximum: 254 }
+    has_secure_password
 
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-   validates :password, length: { minimum: 6 }, allow_blank: true
-   validates :email,
-             presence: true,
-             uniqueness: { case_sensitive: false },
-             length: { minimum: 3, maximum: 254 }
+    enum role: [:member, :admin]
 
-   has_secure_password
-   enum role: [:member, :admin]
+    def favorite_for(post)
+        favorites.where(post_id: post.id).first
+    end
 
-   def format_name
-     if name
-       name_array = []
-       name.split.each do |name_part|
-         name_array << name_part.capitalize
-       end
-       self.name = name_array.join(" ")
-     end
-   end
-
-   def favorite_for(post)
-     favorites.where(post_id: post.id).first
-   end
+    def avatar_url(size)
+        gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+        "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  let(:user) { create(:user) }
+  #let(:user) { create(:user) }
      it { is_expected.to have_many(:posts) }
      it { is_expected.to have_many(:comments) }
      it { is_expected.to have_many(:votes) }
@@ -21,55 +22,56 @@ RSpec.describe User, type: :model do
      it { is_expected.to validate_length_of(:password).is_at_least(6) }
 
      describe "attributes" do
-       it "responds to role" do
-       expect(user).to respond_to(:role)
-     end
+        it "should have name and email attributes" do
+            expect(user).to have_attributes(name: user.name, email: user.email)
+        end
 
-     it "responds to admin?" do
-       expect(user).to respond_to(:admin?)
-     end
+        it "responds to role" do
+            expect(user).to respond_to(:role)
+        end
 
-     it "responds to member?" do
-       expect(user).to respond_to(:member?)
-     end
+        it "responds to admin?" do
+            expect(user).to respond_to(:admin?)
+        end
 
-   describe "roles" do
-     it "is member by default" do
-       expect(user.role).to eql("member")
-     end
+        it "responds to member?" do
+            expect(user).to respond_to(:member?)
+        end
+    end
 
-     context "member user" do
-       it "returns true for #member?" do
-         expect(user.member?).to be_truthy
-       end
+    describe "roles" do
+        it "is member by default" do
+            expect(user.role).to eq("member")
+        end
 
-       it "returns false for #admin?" do
-         expect(user.admin?).to be_falsey
-       end
-     end
+        context "member user" do
+            it "returns true for #member?" do
+                expect(user.member?).to be_truthy
+            end
 
-     context "admin user" do
-       before do
-         user.admin!
-       end
+            it "returns false for #admin?" do
+                expect(user.admin?).to be_falsey
+            end
+        end
 
-       it "returns false for #member?" do
-         expect(user.member?).to be_falsey
-       end
+        context "admin user" do
+            before do
+                user.admin!
+            end
 
-       it "returns true for #admin?" do
-         expect(user.admin?).to be_truthy
-       end
-     end
-   end
-       it "should have name and email attributes" do
-         expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
-       end
-     end
+            it "returns false for #member?" do
+                expect(user.member?).to be_falsey
+            end
+
+            it "returns true for #admin?" do
+                expect(user.admin?).to be_truthy
+            end
+        end
+    end
 
     describe "invalid user" do
-     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+     let(:user_with_invalid_name) { build(:user, name: "") }
+     let(:user_with_invalid_email) { build(:user, email: "") }
 
      it "should be an invalid user due to blank name" do
        expect(user_with_invalid_name).to_not be_valid
@@ -77,12 +79,6 @@ RSpec.describe User, type: :model do
 
      it "should be an invalid user due to blank email" do
        expect(user_with_invalid_email).to_not be_valid
-     end
-
-     it "should format the user's name" do
-       user.name = "bloc user"
-       user.save
-       expect(user.name).to eq "Bloc User"
      end
    end
 
@@ -99,6 +95,20 @@ RSpec.describe User, type: :model do
      it "returns the appropriate favorite if it exists" do
        favorite = user.favorites.where(post: @post).create
        expect(user.favorite_for(@post)).to eq(favorite)
+     end
+
+     it "adds the favorite to the user account" do
+          favorite = user.favorites.where(post: @post).create
+          expect(user.favorites.any?).to be_truthy
+      end
+   end
+
+   describe ".avatar_url" do
+     let(:known_user) { create(:user, email: "blochead@bloc.io") }
+
+     it "returns the proper Gravatar url for a known email entity" do
+       expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+       expect(known_user.avatar_url(48)).to eq(expected_gravatar)
      end
    end
 end
