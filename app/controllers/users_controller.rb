3ff10f1bec46cd16cee73ad1262e176bc7044905
nahome <nahom.teshome@yahoo.com>
2018-03-16 13:26:41 -0700

    signing up assignment redo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,16 @@ class UsersController < ApplicationController
 
      if @user.save
        flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+       create_session(@user)
        redirect_to root_path
      else
        flash.now[:alert] = "There was an error creating your account. Please try again."
        render :new
      end
+   end
+
+   def show
+     @user = User.find(params[:id])
+     @posts = @user.posts.visible_to(current_user)
    end
 end
