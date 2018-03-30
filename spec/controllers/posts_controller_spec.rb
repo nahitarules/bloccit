f8049c5ac1d7267f0d92003b01f7fd41e276eb5e
nahome <nahom.teshome@yahoo.com>
2018-03-29 22:04:40 -0700

    modify specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -471,12 +471,6 @@ RSpec.describe PostsController, type: :controller do
     end
 
     describe "DELETE destroy" do
-      it "deletes the post" do
-        delete :destroy, topic_id: my_topic.id, id: my_post.id
-        count = Post.where({id: my_post.id}).size
-        expect(count).to eq 0
-      end
-
       it "redirects to posts index" do
         delete :destroy, topic_id: my_topic.id, id: my_post.id
         expect(response).to redirect_to my_topic
